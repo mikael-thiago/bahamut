f2c6c5880ebdba0164848b8142362f838078aa78
Mikael <mikael.cerqueira@labsit.io>
2022-09-25 12:41:56 -0300

    Created http adapters scheme and implentation with axios.

---
+++ client/src/adapters/http/api.ts
@@ -0,0 +1,40 @@
+import { AxiosInstance, AxiosResponse } from "axios";
+
+import { api } from "../../axios";
+
+export abstract class HttpAdapter {
+  abstract get<Response = any>(url: string): Promise<Response>;
+  abstract post<Response = any, Body = any>(url: string, body?: Body): Promise<Response>;
+  abstract put<Response = any>(url: string, body?: Body): Promise<Response>;
+  abstract delete<Response = any>(url: string): Promise<Response>;
+}
+
+export class AxiosHttpAdapter implements HttpAdapter {
+  constructor(private _axiosInstance: AxiosInstance) {}
+
+  private _unwrap<T>(promise: Promise<AxiosResponse<T>>): Promise<T> {
+    return promise
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err.response.data;
+      });
+  }
+
+  get<Response = any>(url: string): Promise<Response> {
+    return this._unwrap(this._axiosInstance.get(url));
+  }
+
+  post<Response = any, Body = any>(url: string, body?: Body | undefined): Promise<Response> {
+    return this._unwrap(this._axiosInstance.post(url, body));
+  }
+
+  put<Response = any>(url: string, body?: Body | undefined): Promise<Response> {
+    return this._unwrap(this._axiosInstance.put(url, body));
+  }
+
+  delete<Response = any>(url: string): Promise<Response> {
+    return this._unwrap(this._axiosInstance.delete(url));
+  }
+}
+
+export const httpAdapter: HttpAdapter = new AxiosHttpAdapter(api);
