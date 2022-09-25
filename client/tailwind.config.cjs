526cc2910b10031d5b707a98d99f8852028506a7
Mikael <mikael.cerqueira@labsit.io>
2022-09-25 12:46:34 -0300

    Created aditional sizes for tailwind.

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,22 @@
+const aditionalSizes = {};
+
+for (let i = 0; i < 21; i++) {
+  aditionalSizes[100 + i * 4] = `${25 + i}rem`;
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ["class", ".chakra-ui-dark"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        ...aditionalSizes,
+      },
+      height: {
+        ...aditionalSizes,
+      },
+    },
   },
   plugins: [],
 };
